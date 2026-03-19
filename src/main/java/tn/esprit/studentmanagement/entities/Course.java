b057bcd94f3ba2657cddc20fa6871f52ad4b91d0
Molk Saouabi <58466984+molk95@users.noreply.github.com>
2026-03-19 16:53:45 +0100

    Update Course.java

--- src/main/java/tn/esprit/studentmanagement/entities/Course.java
+++ src/main/java/tn/esprit/studentmanagement/entities/Course.java
@@ -20,9 +20,8 @@ public class Course {
     private int credit;            // nombre de crédits
     private String description;
 
-    @ToString.Exclude
-    @JsonIgnore
     @OneToMany(mappedBy = "course")
+    @JsonManagedReference("course-enrollments")
     private List<Enrollment> enrollments;
 
 }
