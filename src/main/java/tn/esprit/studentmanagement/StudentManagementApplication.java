529801999a9ebec38da8298bdf7c703326c80cb0
molk95 <molk.saouabi@gmail.com>
2026-03-19 21:12:33 +0100

    openai config

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -1,4 +1,5 @@
-package tn.esprit.studentmanagement;
+package tn.esprit.studentmanagement.*;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
